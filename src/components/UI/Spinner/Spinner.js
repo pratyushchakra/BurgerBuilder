9e61371ee043e1e7f000822eb9721c17f28cf70b
Pratyush <pratyushkumarchakra@gmail.com>
2018-11-17 11:45:49 +0530

    routing

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import classes from './Spinner.css'
-const Spinner = () => (
-    <div className={classes.loader}>Loading...</div>
-)
-export default Spinner;
+
+import classes from './Spinner.css';
+
+const spinner = () => (
+    <div className={classes.Loader}>Loading...</div>
+);
+
+export default spinner;
